468377a96b6539b1d03964a99a4ada02903a78f1
Colegio Unifev <matheussantos12010@outlook.com>
2026-03-02 22:40:56 -0300

    noticiasphp

---
+++ noticia.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>STOMPGAMES</title>
+<link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+<header>
+    <h1>STOMPGAMES</h1>
+</header>
+
+<nav>
+    <ul>
+<li><a href="">Home </a></li>
+<li><a href="https://hype.games/br/jogos-grandes-descontos" target="_blank">Promoções de games </a></li>
+    </ul>
+
+</nav>
+
+<main>
+    <br/>
+    <br/>
+    <article>
+        <h2>RESIDENT EVIL REQUIEM FINALMENTE ESTREIOU!</h2>
+        <br/>
+
+        <p>Resident Evil Requiem estreiou nesta sexta-feira (27/02/2026) quebrando recordes e se tornando o maior lançamento da franquia na Steam.</p>
+        <br/>
+
+        <img src="img/Leon.webp" alt="Leon Kennedy">
+        <br/>
+        <a href="https://store.steampowered.com/app/3764200/Resident_Evil_Requiem" target="_blank" rel="noopener noreferrer" class="linkdosite">Comprar agora</a>
+        <br/>
+        <br/>
+        <p class="recuo">Que a capcom estreiou REVIL REQUIEM com o pé direito, já sabemos, mas você já se perguntou o por que da comunidade gostar tanto desse novo lançamento? 
+            Como já haviamos visto no trailer, o game nos apresenta 2 personagens jogáveis: O queridíssimo Agente da DSO Leon Kennedy e a Analista do FBI Grace Ashcroft.</p>
+        <p> 
+            Mantendo seu elemento Clássico de Survival Horror, você Player consegue alternar entre primeira e terceira pessoa. Mas afinal, por qual motivo a comunidade amou este lançamento?
+            REVIL Requiem, não só é um jogo de terror, mas como dito antes, ele é um Survival Horror, onde qualquer decisão errada pode custar a sua vida no game, que pelo o que podemos ver
+            in-game, é bem sinistro.</p>
+        </article>
+        <br/>
+        <br/>
+        <br/>
+        <article class="segunda">
+            <h2>ANTES DO GTA 6, Marvel's Wolverine FINALMENTE GANHA DATA DE LANÇAMENTO!</h2>
+            <br/>
+
+            <p>A Insomniac Games, desenvolvedora de Marvel's Spider-Man 1 e 2, anunciou nesta terça-feira (24) a data de lançamento do seu aguardado jogo, Marvel's Wolverine.
+                 Se você estava preocupado se conseguiria aproveitar a jornada de Logan antes de GTA 6, pode se acalmar que o game de PS5 chegará em 15 de setembro de 2026.</p>
+                 <br/>
+
+                <img src="img/wolverine.jpeg" alt="Wolverine">
+                <br/>
+                <a class="wolve" href="https://www.playstation.com/pt-br/games/marvels-wolverine/" target="_blank" rel="noopener noreferrer"> Confira na loja</a>
+                <br/>
+                <br/>
+                <p class="recuo2">Após rumores e vazamentos, Marvel's Wolverine foi revelado durante o State of Play de setembro de 2025. Na ocasião, o game recebeu um trailer sangrento que confirmou a presença de Mística, Omega Vermelho e alguns outros personagens do X-Men que estarão presentes na trama.
+
+O gameplay foca muito nos combos e finalizações que o Wolverine pode fazer, desde partir inimigos ao meio até cortes limpos com suas afiadas garras.
+
+Com o anúncio da data de lançamento de Marvel's Wolverine para setembro, o mês promete ser promissor para os fãs de PlayStation que, além do jogo da Insomniac, terão também Phantom Blade Zero lançado uma semana antes.</p>
+                 <br/>
+
+
+
+
+
+            
+        </article>
+
+</main>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<footer>
+    
+    
+    <p>&copy;<?php echo date('Y'); ?>-Desenvolvido por Matheus Oliveira.</p>
+    <p>Alguma problema? Reporte! Entre em contato via whatsapp: (17) 99999-9999<p>
+
+</footer>
+
+
+</body>
+</html>
